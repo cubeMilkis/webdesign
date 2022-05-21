c8b2ce398b4ef2fda02e2d990d102dc577dd67a0
BamTori <citypark1281@gmail.com>
2022-05-21 21:50:42 +0800

    B-2 last commit

--- B-2 대한투어/javascript/script.js
+++ B-2 대한투어/javascript/script.js
@@ -7,4 +7,15 @@ jQuery(document).ready(function(){
         $(`.subMenu`).stop().slideUp(300);
         $(`#menuBg`).stop().slideUp(300);
     });
+
+    $(`.imgSlide a:gt(0)`).hide();
+    setInterval(function(){
+        $(`.imgSlide a:first`)
+        .fadeOut(1000)
+        .next(`a`)
+        .fadeIn(1000)
+        .end()
+        .appendTo(`.imgSlide`);
+
+    },3000);
 })
