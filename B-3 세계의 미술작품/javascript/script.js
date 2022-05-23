803ac6ba1b7dcd1d78c7a2a48cf01d8b67193356
BamTori <citypark1281@gmail.com>
2022-05-24 00:50:34 +0800

    Final Commit B-3

--- B-3 세계의 미술작품/javascript/script.js
+++ B-3 세계의 미술작품/javascript/script.js
@@ -1,8 +1,27 @@
 jQuery(document).ready(function(){
     
+    $(`.mainMenu > li`).mouseover(function(){
+        $(this).find(`.subMenu`).stop().slideDown(500);
+    }).mouseout(function(){
+        $(this).find(`.subMenu`).stop().slideUp(500);
+    });
 
 
 
+    setInterval(function(){
+        $(`.slideList`).delay(1000);
+        $(`.slideList`).animate({marginTop: -300});
+        $(`.slideList`).delay(2000);
+        $(`.slideList`).animate({marginTop: -600});
+        $(`.slideList`).delay(2000);
+        $(`.slideList`).animate({marginTop: 0});
+        $(`.slideList`).delay(1000);
+    });
 
-    $(``)
-})
+    $(`.notice li:first-child`).click(function(){
+        $(`#layer`).addClass(`active`);
+    });
+    $(`.closeBtn`).click(function(){
+        $(`#layer`).removeClass(`active`);
+    });
+});
