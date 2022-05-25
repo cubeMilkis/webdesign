2a6f20c30e51814e46e88c5a61a949eb23b048dc
BamTori <citypark1281@gmail.com>
2022-05-25 10:56:37 +0800

    Second Commit C-3

--- C-3 남도맛기행축제/javascript/script.js
+++ C-3 남도맛기행축제/javascript/script.js
@@ -6,5 +6,13 @@ jQuery(document).ready(function(){
         $(this).find(`.subMenu`).stop().slideUp(300);
     });
 
-
+    setInterval(function(){
+        $(`.slideList`).delay(1000)
+        $(`.slideList`).animate({marginLeft: -800})
+        $(`.slideList`).delay(2000)
+        $(`.slideList`).animate({marginLeft: -1600})
+        $(`.slideList`).delay(2000)
+        $(`.slideList`).animate({marginLeft: 0})
+        $(`.slideList`).delay(1000)
+    });
 })
