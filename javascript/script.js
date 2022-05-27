be28f895a66fda204ac0b0646f7e96bce6b6dfbe
BamTori <100062945+cubeMilkis@users.noreply.github.com>
2022-05-28 03:28:44 +0800

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function(){
+    $(`.mainMenu li`).mouseover(function(){
+        $(this).find(`.subMenu`).stop().slideDown(300);
+    }).mouseout(function(){
+        $(this).find(`.subMenu`).stop().slideUp(300);
+    })
+
+    $(`.imgSlide a:gt(0)`).hide();
+    setInterval(function(){
+        $(`.imgSlide a:first-child`)
+        .fadeOut(1000)
+        .next(`a`)
+        .fadeIn(1000)
+        .end()
+        .appendTo(`.imgSlide`)  
+    },3000)
+
+    $(`.notice li:first-child`).click(function(){
+        $(`#modal`).addClass(`active`);
+
+    $(`.modalUp .upBtn`).click(function(){
+        $(`#modal`).removeClass(`active`);
+    })
+    })
+});
