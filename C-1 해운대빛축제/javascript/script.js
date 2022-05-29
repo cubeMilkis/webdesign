af4d2fbd777969bd0b85d6317b18311645f377bf
BamTori <citypark1281@gmail.com>
2022-05-30 03:37:26 +0800

    Last Commit C-1

--- C-1 해운대빛축제/javascript/script.js
+++ C-1 해운대빛축제/javascript/script.js
@@ -4,4 +4,17 @@ jQuery(document).ready(function(){
     }).mouseout(function(){
         $(this).find(`.subMenu`).stop().slideUp(400);
     });
+
+    $(`.imgSlide a:gt(0)`).hide();
+    setInterval(function(){
+        $(`.imgSlide a:first`)
+        .fadeOut(1000)
+        .next(`a`)
+        .fadeIn(1000)
+        .end()
+        .appendTo(`.imgSlide`)
+    },3000);
+
+
+
 });
