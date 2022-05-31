2072801d69525739b4918ca350a0e85468b32ae8
BamTori <citypark1281@gmail.com>
2022-05-31 19:17:09 +0800

    Final Commit A-3

---
+++ A-3 강원천문대/javascript/script.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function(){
+    $(`.menu .mainMenu li`).mouseover(function(){
+        $(this).find(`.subMenu`).stop().slideDown(500);
+    }).mouseout(function(){
+        $(this).find(`.subMenu`).stop().slideUp(500);
+    });
+
+    $(`.imgSlide a:gt(0)`).hide();
+    setInterval(function(){
+        $(`.imgSlide a:first`)
+        .fadeOut(1000)
+        .next(`a`)
+        .fadeIn(1000)
+        .end()
+        .appendTo(`.imgSlide`)
+    },3000);
+
+    $(function(){
+        $(`.tabMenu > li > a`).click(function(){
+            $(this).parent().addClass(`active`)
+            .siblings().removeClass(`active`);
+            return false;
+        });
+    });
+
+    $(`.notice li a:first`).click(function(){
+        $(`#layer`).addClass(`active`)
+    });
+    $(`.upBtn`).click(function(){
+        $(`#layer`).removeClass(`active`)
+    });
+
+})
