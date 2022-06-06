124832d2a7a6d51688de83c4a861a2b8be71fe0a
BamTori <citypark1281@gmail.com>
2022-06-06 21:17:35 +0800

    Final Commit B-4

--- B-4 산업대학교/javascript/script.js
+++ B-4 산업대학교/javascript/script.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function(){
+    //Sub Menu Slide motion
     $(`.menu .mainMenu li a`).mouseover(function(){
         $(`.subMenu`).stop().slideDown(300);
         $(`.menuFill`).stop().slideDown(300);
@@ -7,6 +8,7 @@ jQuery(document).ready(function(){
         $(`.menuFill`).stop().slideUp(300);
     })
 
+    //Image Slide motion
     setInterval(function(){
         $(`.slideList`).delay(1000);
         $(`.slideList`).animate({marginLeft: -1200});
@@ -16,4 +18,13 @@ jQuery(document).ready(function(){
         $(`.slideList`).animate({marginLeft: 0});
         $(`.slideList`).delay(1000);
     });
+
+    //Modal Pop up
+    $(`.notice li:first-child`).click(function(){
+        $(`#modal`).addClass(`active`);
+    });
+    //Modal Close Btn
+    $(`.modalUp .upBtn`).click(function(){
+        $(`#modal`).removeClass(`active`);
+    })
 })
